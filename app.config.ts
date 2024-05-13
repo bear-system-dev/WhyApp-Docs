b2769cc09b190acc2b11974e5d90d848f74c1b8d
devdouglassilva <devdouglassilva@gmail.com>
2024-05-13 14:29:30 +0000

    Chore: Update app.config with project-specific val Replace default values with project-specific configurations

--- app.config.ts
+++ app.config.ts
@@ -1,24 +1,18 @@
 // https://github.com/nuxt-themes/docus/blob/main/nuxt.schema.ts
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    title: 'WhyApp',
+    description: 'WhyApp é um aplicativo de bate-papo projetado para facilitar a comunicação entre os usuários.',
+    image: 'favicon.png',
     socials: {
-      twitter: 'nuxt_js',
-      github: 'nuxt-themes/docus',
-      nuxt: {
-        label: 'Nuxt',
-        icon: 'simple-icons:nuxtdotjs',
-        href: 'https://nuxt.com'
-      }
+      github: 'https://github.com/bear-system-dev/WhyApp-Docs',
     },
     github: {
       dir: '.starters/default/content',
       branch: 'main',
-      repo: 'docus',
-      owner: 'nuxt-themes',
-      edit: true
+      repo: 'WhyApp-Docs',
+      owner: 'Bear System',
+      edit: false
     },
     aside: {
       level: 0,
@@ -34,6 +28,6 @@ export default defineAppConfig({
       showLinkIcon: true,
       exclude: [],
       fluid: true
-    }
+    } 
   }
 })
